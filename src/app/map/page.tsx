e8c1c073f44ba4bd582cbe838d30097b3927b9ba
Jeff Claybrook <jeffclaybrook@Jeffs-MacBook-Pro.local>
2025-02-01 13:08:10 -0600

    Added leads map

--- src/app/map/page.tsx
+++ src/app/map/page.tsx
@@ -1,13 +1,14 @@
 import { Suspense } from "react"
 import { Header } from "@/components/header"
+import { LeadsMap } from "@/components/leads-map"
 import { Loader } from "@/components/loader"
 
 export default function Map() { 
  return (
-  <main className="flex flex-1 flex-col w-full pb-4">
+  <main className="flex flex-1 flex-col w-full">
    <Header title="Map" />
    <Suspense fallback={<Loader />}>
-    <Loader />
+    <LeadsMap />
    </Suspense>
   </main>
  )
